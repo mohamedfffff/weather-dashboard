0efd92b95d63635d3ce68c9f040e7b14915825e3
the thing <65288778+mohamedfffff@users.noreply.github.com>
2026-05-13 14:51:10 +0300

    setup for ci/cd

--- src/test/java/com/example/lusterz/weather_dashboard/integration/WeatherServiceTest.java
+++ src/test/java/com/example/lusterz/weather_dashboard/integration/WeatherServiceTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,6 +15,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class WeatherServiceTest {
 
     @Autowired
